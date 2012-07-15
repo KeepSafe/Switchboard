e92af9b6d8d4c109f997713bd76e5d7aea0d3d13
Philipp Berner <philipp@appbackr.com>
2012-07-14 18:55:04 -0700

    changed server location for example app

--- server/php/excample/htdocs/currentServerUrl.php
+++ server/php/excample/htdocs/currentServerUrl.php
@@ -2,10 +2,10 @@
 
 $returnArray = array(
 	//path to this file
-	'updateServerUrl' => "http://domain/path_to/currentServerUrl.php",
+	'updateServerUrl' => "http://switchboard.herokuapp.com/currentServerUrl.php",
 	
 	//path to config.php file where the actual dynamic config is generated
-	'configServerUrl' => "http://domain/path_to/config.php"
+	'configServerUrl' => "http://switchboard.herokuapp.com/config.php"
 );
 
 header('Content-type: application/json');
